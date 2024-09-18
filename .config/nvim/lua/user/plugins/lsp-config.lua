474b88f6da0c4031226f5f7635a3251cfaabb4ad
G M T Borges <gustavo@gmtborges.com>
2024-09-17 21:38:40 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/lsp-config.lua
+++ .config/nvim/lua/user/plugins/lsp-config.lua
@@ -185,10 +185,10 @@ return {
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           local opts = { buffer = ev.buf }
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
           vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
           vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
           vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
