d48425cff5e5a8753945989aaf601ed516ce27ab
G M T Borges <gustavo@gmtborges.com>
2024-10-03 16:47:51 -0300

    atualiza dotfiles

---
+++ .config/nvim/lua/user/plugins/xcodebuild.lua
@@ -0,0 +1,18 @@
+return {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-tree.lua",         -- (optional) to manage project files
+    "stevearc/oil.nvim",               -- (optional) to manage project files
+    "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+  },
+  config = function()
+    require("xcodebuild").setup({
+    })
+    vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
+    vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
+    vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+    vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show All Xcodebuild Actions" })
+  end,
+}
