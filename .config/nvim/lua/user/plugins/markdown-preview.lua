7bd64dfb9bea8fd0a5064516a12b0846cdac598d
G M T Borges <gustavo@gmtborges.com>
2024-10-09 21:30:31 -0300

    fix sourcekit because of nvim-highlight-colors

--- .config/nvim/lua/user/plugins/markdown-preview.lua
+++ .config/nvim/lua/user/plugins/markdown-preview.lua
@@ -1,13 +1,11 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		config = function()
-			vim.keymap.set("n", "<leader>tm", ":MarkdownPreviewToggle<CR>")
-		end,
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    config = function()
+      vim.keymap.set("n", "<leader>tm", ":MarkdownPreviewToggle<CR>")
+    end,
+  },
 }
--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -4,37 +4,6 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local function list_registered_providers_names(filetype)
-        local s = require("null-ls.sources")
-        local available_sources = s.get_available(filetype)
-        local registered = {}
-        for _, source in ipairs(available_sources) do
-          for method in pairs(source.methods) do
-            registered[method] = registered[method] or {}
-            table.insert(registered[method], source.name)
-          end
-        end
-        return registered
-      end
-
-      -- local function list_registered_formatters(filetype)
-      -- 	local method = require("null-ls").methods.FORMATTING
-      -- 	local registered_providers = list_registered_providers_names(filetype)
-      -- 	return registered_providers[method] or {}
-      -- end
-      --
-      -- local function list_registered_linters(filetype)
-      -- 	local method = require("null-ls").methods.DIAGNOSTICS
-      -- 	local registered_providers = list_registered_providers_names(filetype)
-      -- 	return registered_providers[method] or {}
-      -- end
-      --
-      -- local function list_registered_hovers(filetype)
-      -- 	local method = require("null-ls").methods.HOVER
-      -- 	local registered_providers = list_registered_providers_names(filetype)
-      -- 	return registered_providers[method] or {}
-      -- end
-
       local function lsp_client(msg)
         msg = msg or ""
         local buf_clients = vim.lsp.get_clients()
@@ -45,7 +14,6 @@ return {
           return msg
         end
 
-        local buf_ft = vim.bo.filetype
         local buf_client_names = {}
 
         -- add client
@@ -54,18 +22,6 @@ return {
             table.insert(buf_client_names, client.name)
           end
         end
-
-        -- add formatter
-        -- local supported_formatters = list_registered_formatters(buf_ft)
-        -- vim.list_extend(buf_client_names, supported_formatters)
-
-        -- add linter
-        -- local supported_linters = list_registered_linters(buf_ft)
-        -- vim.list_extend(buf_client_names, supported_linters)
-
-        -- add hover
-        -- local supported_hovers = list_registered_hovers(buf_ft)
-        -- vim.list_extend(buf_client_names, supported_hovers)
 
         return "[" .. table.concat(buf_client_names, ", ") .. "]"
       end
@@ -130,35 +86,6 @@ return {
       vim.keymap.set("n", "<leader>bp", ":BufferLineMovePrev<CR>")
     end,
   },
-  -- {
-  -- 	"stevearc/aerial.nvim",
-  -- 	opts = {},
-  -- 	-- Optional dependencies
-  -- 	dependencies = {
-  -- 		"nvim-treesitter/nvim-treesitter",
-  -- 		"nvim-tree/nvim-web-devicons",
-  -- 	},
-  -- 	config = function()
-  -- 		require("aerial").setup({
-  -- 			backends = { "lsp" },
-  -- 			filter_kind = { "Function", "Constant" },
-  -- 			layout = {
-  -- 				default_direction = "prefer_left",
-  -- 				min_width = 30,
-  -- 			},
-  -- 			open_automatic = false,
-  -- 			highlight_on_jump = false,
-  -- 			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
-  -- 			on_attach = function(bufnr)
-  -- 				-- Jump forwards/backwards with '{' and '}'
-  -- 				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-  -- 				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-  -- 			end,
-  -- 		})
-  -- 		-- You probably also want to set a keymap to toggle aerial
-  -- 		vim.keymap.set("n", "<leader>s", "<cmd>AerialToggle!<CR>")
-  -- 	end,
-  -- },
   {
     "lukas-reineke/indent-blankline.nvim",
     enabled = true,
@@ -204,79 +131,87 @@ return {
   },
   {
     "brenoprata10/nvim-highlight-colors",
+    enabled = true,
+    cmd = "HighlightColors",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("nvim-highlight-colors").setup({
+      local hc = require("nvim-highlight-colors")
+      hc.setup({
+        render = "virtual",
         enable_tailwind = true,
+        exclude_filetypes = { "swift" }
+      })
+      hc.turnOff()
+    end,
+    vim.keymap.set("n", "<leader>tc", ":HighlightColors Toggle<CR>")
+  },
+  {
+    "glepnir/dashboard-nvim",
+    enabled = false,
+    config = function()
+      local custom_header = {
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[  ██████   █████                   █████   █████  ███                  ]],
+        [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
+        [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
+        [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
+        [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
+        [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
+        [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
+        [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+        [[                                                                       ]],
+      }
+      local custom_center = {
+        {
+          icon = "  ",
+          desc = "Find files                     ",
+          key = "SPC p",
+          action = function()
+            require("telescope.builtin").find_files({ previewer = false })
+          end,
+        },
+        {
+          icon = "  ",
+          desc = "Open explorer                  ",
+          key = "SPC e",
+          action = ":NvimTreeToggle<CR>",
+        },
+        {
+          icon = "  ",
+          desc = "Find word                     ",
+          key = "SPC f w",
+          action = function()
+            require("telescope.builtin").live_grep()
+          end,
+        },
+        {
+          icon = "  ",
+          desc = "Find recent files             ",
+          key = "SPC f r",
+          action = function()
+            require("telescope.builtin").oldfiles()
+          end,
+        },
+      }
+      local custom_footer = {
+        [[ ]],
+        [[ ]],
+        "One day at a time  ",
+      }
+      require("dashboard").setup({
+        theme = "doom",
+        config = {
+          header = custom_header,
+          center = custom_center,
+          footer = custom_footer,
+        },
       })
     end,
   },
-  -- {
-  -- 	"glepnir/dashboard-nvim",
-  -- 	config = function()
-  -- 		local custom_header = {
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 			[[  ██████   █████                   █████   █████  ███                  ]],
-  -- 			[[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
-  -- 			[[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
-  -- 			[[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
-  -- 			[[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
-  -- 			[[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
-  -- 			[[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
-  -- 			[[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 			[[                                                                       ]],
-  -- 		}
-  -- 		local custom_center = {
-  -- 			{
-  -- 				icon = "  ",
-  -- 				desc = "Find files                     ",
-  -- 				key = "SPC p",
-  -- 				action = function()
-  -- 					require("telescope.builtin").find_files({ previewer = false })
-  -- 				end,
-  -- 			},
-  -- 			{
-  -- 				icon = "  ",
-  -- 				desc = "Open explorer                  ",
-  -- 				key = "SPC e",
-  -- 				action = ":NvimTreeToggle<CR>",
-  -- 			},
-  -- 			{
-  -- 				icon = "  ",
-  -- 				desc = "Find word                     ",
-  -- 				key = "SPC f w",
-  -- 				action = function()
-  -- 					require("telescope.builtin").live_grep()
-  -- 				end,
-  -- 			},
-  -- 			{
-  -- 				icon = "  ",
-  -- 				desc = "Find recent files             ",
-  -- 				key = "SPC f r",
-  -- 				action = function()
-  -- 					require("telescope.builtin").oldfiles()
-  -- 				end,
-  -- 			},
-  -- 		}
-  -- 		local custom_footer = {
-  -- 			[[ ]],
-  -- 			[[ ]],
-  -- 			"One day at a time  ",
-  -- 		}
-  -- 		require("dashboard").setup({
-  -- 			theme = "doom",
-  -- 			config = {
-  -- 				header = custom_header,
-  -- 				center = custom_center,
-  -- 				footer = custom_footer,
-  -- 			},
-  -- 		})
-  -- 	end,
-  -- },
 }
