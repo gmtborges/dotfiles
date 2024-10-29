42b8086789f84c8443f488a9e479e8669daa84c1
Gustavo Martins T Borges <gustavo@gmtborges.com>
2024-10-28 23:24:00 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/markdown-preview.lua
+++ .config/nvim/lua/user/plugins/markdown-preview.lua
@@ -1,9 +1,13 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
+    enabled = true,
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
     config = function()
       vim.keymap.set("n", "<leader>tm", ":MarkdownPreviewToggle<CR>")
     end,
