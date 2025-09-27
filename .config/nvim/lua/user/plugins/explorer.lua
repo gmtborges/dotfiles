d80419da501b1ae4a089cabca9c86c631b0768b0
Gustavo Borges <gustavo@gbsoft.net>
2025-09-27 11:53:43 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/explorer.lua
+++ .config/nvim/lua/user/plugins/explorer.lua
@@ -1,46 +1,38 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    enabled = true,
-    version = "*",
-    config = function()
-      local files = require("mini.files")
-      files.setup({
-        mappings = {
-          synchronize = "<C-s>",
-        },
-      })
+	{
+		"echasnovski/mini.nvim",
+		enabled = true,
+		version = "*",
+		config = function()
+			local files = require("mini.files")
+			files.setup({
+				mappings = {
+					synchronize = "<C-s>",
+				},
+			})
 
-      vim.keymap.set("n", "<leader>fe", function()
-        if not files.close() then
-          files.open(vim.api.nvim_buf_get_name(0))
-          files.reveal_cwd()
-        end
-      end)
-    end,
-  },
-  {
-    "A7Lavinraj/fyler.nvim",
-    enabled = true,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- branch = "stable",
-    opts = {
-      icon_provider = "nvim_web_devicons"
-    },
-    config = function()
-      require("fyler").setup({
-        views = {
-          explorer = {
-            -- width = 0.3,
-            -- kind = "split:rightmost"
-            win = {
-              kind = "split_right_most",
-            }
-          }
-        }
-
-      })
-      vim.keymap.set("n", "<leader>e", ":Fyler kind=split_right_most<CR>")
-    end
-  }
+			vim.keymap.set("n", "<leader>fe", function()
+				if not files.close() then
+					files.open(vim.api.nvim_buf_get_name(0))
+					files.reveal_cwd()
+				end
+			end)
+		end,
+	},
+	{
+		"A7Lavinraj/fyler.nvim",
+		enabled = true,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		commit = "028830a",
+		opts = {},
+		config = function()
+			require("fyler").setup({
+				icon_provider = "nvim_web_devicons",
+				win = {
+					kind = "split_right_most",
+				},
+			})
+			vim.keymap.set("n", "<leader>e", ":Fyler kind=split_right_most<CR>")
+		end,
+	},
 }
