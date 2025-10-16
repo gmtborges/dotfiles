1fedbcbc4bdf1cfba86cada59cae64207999d930
Gustavo Borges <gustavo@gbsoft.net>
2025-10-16 12:51:08 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/editor-utils.lua
+++ .config/nvim/lua/user/plugins/editor-utils.lua
@@ -1,5 +1,16 @@
 return {
 	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup({ enable_builtin = true })
+		end,
+	},
+	{
 		"rest-nvim/rest.nvim",
 		enabled = true,
 		dependencies = {
