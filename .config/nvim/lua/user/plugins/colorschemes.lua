35503562c8357eeb33bb68551f330c96e5a22f6f
Gustavo Borges <gustavo@gbsoft.net>
2025-11-07 15:37:24 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/colorschemes.lua
+++ .config/nvim/lua/user/plugins/colorschemes.lua
@@ -84,14 +84,12 @@ return {
 	{
 		"f-person/auto-dark-mode.nvim",
 		opts = {
-			update_interval = 1000,
+			update_interval = 3000,
 			set_dark_mode = function()
-				vim.opt.background = "dark"
-				vim.cmd("colorscheme catppuccin-macchiato")
+				vim.api.nvim_set_option_value("background", "dark", {})
 			end,
 			set_light_mode = function()
-				vim.opt.background = "light"
-				vim.cmd("colorscheme catppuccin-latte")
+				vim.api.nvim_set_option_value("background", "light", {})
 			end,
 		},
 	},
