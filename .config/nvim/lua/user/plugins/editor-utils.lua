cf53bcc3c7b9e033d8f668baa07732deaa71bbb4
Gustavo Borges <gustavo@gbsoft.net>
2025-12-02 10:59:02 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/editor-utils.lua
+++ .config/nvim/lua/user/plugins/editor-utils.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"pwntester/octo.nvim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("octo").setup({ enable_builtin = true })
-		end,
-	},
-	{
 		"rest-nvim/rest.nvim",
 		enabled = true,
 		dependencies = {
--- .config/nvim/lua/user/plugins/git.lua
+++ .config/nvim/lua/user/plugins/git.lua
@@ -1,57 +1,65 @@
 return {
-  {
-    "sindrets/diffview.nvim",
-  },
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>sg", ":G ")
-    end,
-  },
-  {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = function()
-      require("git-conflict").setup({
-        default_mappings = {
-          next = "]x",
-          prev = "[x",
-        },
-      })
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup({ enable_builtin = true })
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		config = function()
+			require("git-conflict").setup({
+				default_mappings = {
+					next = "]x",
+					prev = "[x",
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gx", ":GitConflictListQf<CR>")
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("gitsigns").setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
+			vim.keymap.set("n", "<leader>gx", ":GitConflictListQf<CR>")
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
 
-          vim.keymap.set("n", "[g", gs.prev_hunk, { buffer = bufnr })
-          vim.keymap.set("n", "]g", gs.next_hunk, { buffer = bufnr })
+					vim.keymap.set("n", "[g", gs.prev_hunk, { buffer = bufnr })
+					vim.keymap.set("n", "]g", gs.next_hunk, { buffer = bufnr })
 
-          vim.keymap.set({ "n", "v" }, "<leader>ga", function()
-            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end, { buffer = bufnr })
-          vim.keymap.set({ "n", "v" }, "<leader>gr", function()
-            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end, { buffer = bufnr })
+					vim.keymap.set({ "n", "v" }, "<leader>ga", function()
+						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { buffer = bufnr })
+					vim.keymap.set({ "n", "v" }, "<leader>gr", function()
+						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { buffer = bufnr })
 
-          vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame)
+					vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame)
 
-          vim.keymap.set("n", "<leader>gA", gs.stage_buffer, { buffer = bufnr })
-          vim.keymap.set("n", "<leader>gR", gs.reset_buffer, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gA", gs.stage_buffer, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gR", gs.reset_buffer, { buffer = bufnr })
 
-          vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { buffer = bufnr })
-          vim.keymap.set("n", "<leader>gd", gs.diffthis, { buffer = bufnr })
-          vim.keymap.set("n", "<leader>gD", function()
-            gs.diffthis("~")
-          end, { buffer = bufnr })
-        end,
-      })
-    end,
-  },
+					vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gd", gs.diffthis, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gD", function()
+						gs.diffthis("~")
+					end, { buffer = bufnr })
+				end,
+			})
+		end,
+	},
 }
