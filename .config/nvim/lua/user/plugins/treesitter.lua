a62d19eee11a433f0aaff14ec5c96fef3233b6e6
Gustavo Borges <gustavo@gbsoft.net>
2025-12-23 14:05:10 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -64,8 +64,8 @@ return {
 		config = function()
 			require("treesitter-context").setup({
 				enable = true,
-				max_lines = 3,
-				zindex = 1,
+				-- max_lines = 3,
+				-- zindex = 1,
 			})
 		end,
 	},
