5eee96c59088ef3d515d584392502ef807937ea4
Gustavo Martins T Borges <gustavo.mtborges@gmail.com>
2026-02-03 21:54:48 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/colorschemes.lua
+++ .config/nvim/lua/user/plugins/colorschemes.lua
@@ -8,7 +8,7 @@ return {
 				transparent = false,
 				style = "storm",
 				styles = {
-					keywords = { bold = true },
+					keywords = { bold = true, italic = false },
 					-- sidebars = "transparent",
 					-- floats = "transparent",
 				},
